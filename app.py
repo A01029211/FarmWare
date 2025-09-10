5b5d87867e82c53ee9a26e2cec550b2a582af368
DavidOrtegit123 <a01286040@tec.mx>
2025-09-10 13:59:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def get_image(filename):
     return send_file(io.BytesIO(result[0]), mimetype=mime_type)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3001, debug=True)
